ef5f8ec611197efcc6d0a802c32d3f06bf17eaeb
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2023-08-07 15:57:00 +0500

    created required models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import *
+
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Deal)
+admin.site.register(Payment)
+admin.site.register(Branch)
+admin.site.register(District)
+admin.site.register(Region)
+admin.site.register(City)
+admin.site.register(OrderDetails)
+admin.site.register(PaymentType)
+admin.site.register(Currency)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,110 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        abstract = True
+
+class Branch(BaseModel):
+    id = models.UUIDField(unique=True, default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=255)
+    smartup_id = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return f'{self.name} | {self.smartup_id}'
+
+class Region(BaseModel):
+    id = models.UUIDField(unique=True, default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+
+class City(BaseModel):
+    id = models.UUIDField(unique=True, default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=255)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class District(BaseModel):
+    id = models.UUIDField(unique=True, default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=255)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True)
+    
+    def __str__(self):
+        return f'{self.name} | {self.city.name}'
+
+
+class User(BaseModel):
+    id = models.UUIDField(unique=True, default=uuid.uuid4, primary_key=True)
+    smartup_id = models.CharField(max_length=255, )
+    name = models.CharField(max_length=255, )
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    
+    address = models.CharField(max_length=255, null=True, blank=True)
+    district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return f'{self.smartup_id} | {self.name}'
+
+class Product(BaseModel):
+    id = models.UUIDField(unique=True, default=uuid.uuid4, primary_key=True)
+    smartup_id = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, )
+    code = models.CharField(max_length=255, )
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    
+    def __str__(self) -> str:
+        return f'{self.name} | {self.smartup_id}'
+
+class Deal(BaseModel):
+    id = models.UUIDField(unique=True, default=uuid.uuid4, primary_key=True)
+    smartup_id = models.CharField(max_length=255, )
+    total = models.CharField(max_length=255, )
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, )
+    
+    def __str__(self) -> str:
+        return f'{self.smartup_id} | {self.customer.name}'
+
+class OrderDetails(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    deal = models.ForeignKey(Deal, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    unit_price = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f'Order for {self.quantity} {self.product.name} in Deal {self.deal.smartup_id}'
+
+class PaymentType(BaseModel):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    name = models.CharField(max_length=255)
+    smartup_id = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Currency(BaseModel):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    name = models.CharField(max_length=255)
+    smartup_id = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Payment(BaseModel):
+    id = models.UUIDField(unique=True, default=uuid.uuid4, primary_key=True)
+    smartup_id = models.CharField(max_length=255, )
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, )
+    currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, null=True, blank=True)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    base_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    
+    def __str__(self) -> str:
+        return 
