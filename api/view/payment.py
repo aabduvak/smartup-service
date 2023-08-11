1326e58b3bf25e2d2bfdd8a78ed08132096c52ba
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2023-08-11 18:39:49 +0500

    added filter to PaymentListView

--- api/view/payment.py
+++ api/view/payment.py
@@ -21,16 +21,32 @@ class PaymentListView(ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
     
-    # def get_queryset(self):
-    #     user_id = self.request.query_params.get('smartup_id')
-    #     branch = self.request.query_params.get('branch_id')
-    #     date = self.request.query_params.get('date')
+    def get_queryset(self):
+        queryset = Payment.objects.all()
         
-    #     if user_id:
-    #         return Payment.objects.filter(smartup_id=user_id)
-    #     elif branch:
-    #         return Payment.objects.all()
-
+        user_id = self.request.query_params.get('customer')
+        branch = self.request.query_params.get('branch')
+        date = self.request.query_params.get('date')
+        currency = self.request.query_params.get('currency')
+        payment_type = self.request.query_params.get('payment_type')
+                
+        if user_id and User.objects.filter(smartup_id=user_id).exists():
+            queryset = queryset.filter(customer__smartup_id=user_id)
+        
+        if branch and Branch.objects.filter(smartup_id=branch).exists():
+            queryset = queryset.filter(branch__smartup_id=branch)
+        
+        if date:
+            date_of_payment = datetime.strptime(date, '%d.%m.%Y')
+            queryset = queryset.filter(date_of_payment=date_of_payment.strftime('%Y-%m-%d'))
+        
+        if currency:
+            queryset = queryset.filter(payment_type__currency__name=currency)
+        
+        if payment_type:
+            queryset = queryset.filter(payment_type__smartup_id=payment_type)
+        
+        return queryset
 
 
 class PaymentDetailView(APIView):
@@ -81,45 +97,46 @@ class CreatePaymentView(PaymentDetailView):
         if response.status_code != 200:
             return Response(status=response.status_code)
         
-        parser = etree.XMLParser(recover=True)
-        root = etree.fromstring(response.content, parser=parser)
+        try:
+            parser = etree.XMLParser(recover=True)
+            root = etree.fromstring(response.content, parser=parser)
 
-        payments = root.xpath("//Оплата")
-        
-        for payment in payments:
-            info = {
-                "customer_id": payment.find("ИдКонтрагента").text,
-                "payment_id": payment.find("ИдОплаты").text,
-                "payment_type_id": payment.find("ИдТипаОплаты").text,
-                "amount": payment.find("Сумма").text,
-                "base_amount": payment.find("Базовая").text,
-                "date_of_payment": payment.find("ДатаОплаты").text,
-            }
+            payments = root.xpath("//Оплата")
+            
+            for payment in payments:
+                info = {
+                    "customer_id": payment.find("ИдКонтрагента").text,
+                    "payment_id": payment.find("ИдОплаты").text,
+                    "payment_type_id": payment.find("ИдТипаОплаты").text,
+                    "amount": payment.find("Сумма").text,
+                    "base_amount": payment.find("Базовая").text,
+                    "date_of_payment": payment.find("ДатаОплаты").text,
+                }
 
-            if Payment.objects.filter(smartup_id=info['payment_id']).exists():
-                continue
+                if Payment.objects.filter(smartup_id=info['payment_id']).exists():
+                    continue
+                
+                if not User.objects.filter(smartup_id=info['customer_id']).exists():
+                    create_customer(info['customer_id'])
+                
+                user = User.objects.get(smartup_id=info['customer_id'])
+                payment_type = PaymentType.objects.get(smartup_id=info['payment_type_id'])
+                amount =  Decimal(info['amount'])
+                base_amount = Decimal(info['base_amount'])
+                date_of_payment = datetime.strptime(info['date_of_payment'], '%d.%m.%Y').date()
+                
+                payment = Payment.objects.create(
+                    smartup_id=info['payment_id'],
+                    customer=user,
+                    payment_type=payment_type,
+                    amount=amount,
+                    base_amount=base_amount,
+                    date_of_payment=date_of_payment.strftime('%Y-%m-%d'),
+                    branch=branch,
+                )
             
-            if not User.objects.filter(smartup_id=info['customer_id']).exists():
-                create_customer(info['customer_id'])
-            
-            user = User.objects.get(smartup_id=info['customer_id'])
-            payment_type = PaymentType.objects.get(smartup_id=info['payment_type_id'])
-            amount =  Decimal(info['amount'])
-            base_amount = Decimal(info['base_amount'])
-            date_of_payment = datetime.strptime(info['date_of_payment'], '%d.%m.%Y').date()
-            
-            payment = Payment.objects.create(
-                smartup_id=info['payment_id'],
-                customer=user,
-                payment_type=payment_type,
-                amount=amount,
-                base_amount=base_amount,
-                date_of_payment=date_of_payment.strftime('%Y-%m-%d'),
-                branch=branch,
-            )
-        
-        payments = Payment.objects.all()
-        serializer = PaymentSerializer(payments, many=True) 
-        return Response(data=serializer.data)
-        # except:
-        #     return Response(status=500)
+            payments = Payment.objects.all()
+            serializer = PaymentSerializer(payments, many=True) 
+            return Response(data=serializer.data)
+        except:
+            return Response(status=500)
