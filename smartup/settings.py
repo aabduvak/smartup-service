68eeddbc74f1cef741620bc30366463861377404
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2023-08-25 11:49:06 +0500

    removed browseable api from drf

--- smartup/settings.py
+++ smartup/settings.py
@@ -184,6 +184,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 50,
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
 }
 
 # Static files (CSS, JavaScript, Images)
